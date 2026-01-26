adf5062e0de39b02e1b64c00560c3c26ecca3b33
nhittm <134468174+tranthimynhi@users.noreply.github.com>
2026-01-26 22:41:55 +0700

    create player class

---
+++ Caro/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caro
+{
+    public class Player
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+        private Image mark;
+        public Image Mark
+        {
+            get { return mark; }
+            set { mark = value; }
+        }
+    }
+}
