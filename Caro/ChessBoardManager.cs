86bb529adfaf9a808a3822832868764ce5013e5a
Admin <Admin@TRNCNGKHANH>
2026-01-27 21:33:04 +0700

    Khanh-Chessboardmanager

---
+++ Caro/ChessBoardManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caro
+{
+    public class ChessBoardManager
+    {
+        #region Properties
+        private Panel chessBoard;
+
+        public Panel ChessBoard
+        {
+            get { return chessBoard; }
+            set { chessBoard = value; }
+        }
+
+
+
+        private List<Player> player;
+        public List<Player> Player
+        {
+            get { return player; }
+            set { player = value; }
+        }
+        private int currentPlayer;
+        public int CurrentPlayer
+        {
+            get { return currentPlayer; }
+            set { currentPlayer = value; }
+        }
+        private TextBox playerName;
+        public TextBox PlayerName
+        {
+            get { return playerName; }
+            set { playerName = value; }
+        }
+        private PictureBox playerMark;
+        public PictureBox PlayerMark
+        {
+            get { return playerMark; }
+            set { playerMark = value; }
+        }
+    }
+}
